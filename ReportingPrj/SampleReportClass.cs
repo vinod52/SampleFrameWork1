7412bac1ea2c24ad3740a17bfefce1d6e7108b17
Vinod Kumar Thotakuri <vinodkumarvinni@gmail.com>
2020-06-14 22:08:12 +0530

    Solution Clean-up and Sample Report Project addition with test case.

---
+++ ReportingPrj/SampleReportClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ReportingPrj
+{
+    [TestClass]
+    public class SampleReportClass
+    {
+        IWebDriver driver = null;
+        ExtentReports extent = null;
+        [TestInitialize]
+        public void ExtentStart()
+        {
+            extent  = new ExtentReports();
+            var htmlReporter = new ExtentHtmlReporter(@"E:\VisualStudio\SampleFrameWork1\ReportingPrj\ExtentReports\Sample.html");
+            extent.AttachReporter(htmlReporter);
+        }
+        [TestCleanup]
+        public void ExtentClose()
+        {
+            extent.Flush();
+        }
+        [TestMethod]
+        public void SampleReport()
+        {
+            ExtentTest test=extent.CreateTest("SampleReport").Info("Test Started");
+            var outputDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            driver= new ChromeDriver(outputDirectory);
+            driver.Manage().Window.Maximize();
+            test.Log(Status.Info,"Chrome Browser Launched");
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+            driver.FindElement(By.Id("email")).SendKeys("reporting");
+            test.Log(Status.Info,"Email id is entered");            
+            driver.Quit();
+            test.Log(Status.Info, "Test1 Passed");
+        }
+    }
+}
